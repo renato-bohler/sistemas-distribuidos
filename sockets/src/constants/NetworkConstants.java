c04d1ca7093a87a723b15c59cb79d4f4ffdaa6e4
Renato Böhler <renato.bohler@gmail.com>
2018-09-06 07:19:54 -0300

    altera a constante de timeout para 5 segundos

--- sockets/src/constants/NetworkConstants.java
+++ sockets/src/constants/NetworkConstants.java
@@ -4,5 +4,5 @@ public final class NetworkConstants {
 	public static final String IP = "233.233.233";
 	public static final int PORT = 6789;
 	public static final int MINIMUM_PEERS = 3;
-	public static final int TIMEOUT_MILLIS = 10000;
+	public static final int TIMEOUT_MILLIS = 5000;
 }
