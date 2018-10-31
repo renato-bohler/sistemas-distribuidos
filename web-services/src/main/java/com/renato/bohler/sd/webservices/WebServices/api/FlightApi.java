1f4f3fc288222a43c967a3342eec5245feeea05f
Renato Böhler <renato.bohler@gmail.com>
2018-10-31 17:59:28 -0200

    implementa classe de API de vôo

---
+++ web-services/src/main/java/com/renato/bohler/sd/webservices/WebServices/api/FlightApi.java
@@ -0,0 +1,54 @@
+package com.renato.bohler.sd.webservices.WebServices.api;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class FlightApi implements Serializable {
+
+	private Long id;
+	private String origem;
+	private String destino;
+	private String data;
+	private Long vagas;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Long getVagas() {
+		return vagas;
+	}
+
+	public void setVagas(Long vagas) {
+		this.vagas = vagas;
+	}
+
+}
